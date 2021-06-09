67fce9b01d304499ec1191a66cb64d43770f9715
Lelxuch <nakyp.sanzhar@gmail.com>
2021-06-09 14:50:56 +0600

    github pages

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -8,27 +8,35 @@ const Projects = () => {
             "id": 1,
             "title": "Software Test Engineer IV",
             "description": "lacus purus aliquet at feugiat non pretium quis lectus suspendisse potenti in eleifend quam a odio in hac habitasse platea dictumst maecenas ut massa quis augue luctus tincidunt nulla mollis"
-          }, {
+        }, {
             "id": 2,
             "title": "Systems Administrator I",
             "description": "duis consequat dui nec nisi volutpat eleifend donec ut dolor morbi vel lectus in quam fringilla rhoncus mauris enim leo rhoncus sed vestibulum sit amet cursus id"
-          }, {
+        }, {
             "id": 3,
             "title": "General Manager",
             "description": "cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus etiam vel augue vestibulum rutrum rutrum neque aenean auctor gravida sem praesent"
-          }, {
+        }, {
             "id": 4,
             "title": "Assistant Manager",
             "description": "elementum in hac habitasse platea dictumst morbi vestibulum velit id pretium iaculis diam erat fermentum justo nec condimentum neque sapien placerat"
-          }, {
+        }, {
             "id": 5,
             "title": "Biostatistician III",
             "description": "donec pharetra magna vestibulum aliquet ultrices erat tortor sollicitudin mi sit amet lobortis sapien sapien non mi integer ac neque"
-          }]
+        }]
     )
     
     return (
-        <div>
+        <div className="projects">
+            <div className="projects-filter">
+                <select name="projectsSelect" id="projectsSelect">
+                    <option value="Submited">Submitted</option>
+                    <option value="InProcess">In Process</option>
+                    <option value="Past">Past</option>
+                    <option value="Denied">Denied</option>
+                </select>
+            </div>
             {projects.map((project) => (
                 <Project_item key={ project.id } project={ project }/>
             ))}
